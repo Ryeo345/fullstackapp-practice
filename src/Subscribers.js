9a4f930f7d894be1c9c2316541564be6131a5f56
Philip Ruan <buddhaphilip2015@gmail.com>
2023-03-24 14:46:19 -0400

    created react component and used redux thunks

--- src/Subscribers.js
+++ src/Subscribers.js
@@ -1,6 +1,19 @@
 import React from 'react';
-
+import { useSelector} from 'react-redux';
 const Subscribers = () => {
-    return <hr />;
+    const {subscribers} = useSelector(state => state);
+    return (
+        <ul>
+            {
+                subscribers.map(subscriber => {
+                    return (
+                        <li key={subscriber.id}>
+                            {subscriber.name}
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    );
 }
 export default Subscribers;
